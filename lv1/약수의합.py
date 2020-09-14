5fc4ee8f9ae232f0c552eb24565bb28333060f86
1pha <62973169+1pha@users.noreply.github.com>
2020-09-14 10:11:50 +0900

    [Update] 약수의합 Add if __name__

--- lv1/약수의합.py
+++ lv1/약수의합.py
@@ -1,2 +1,6 @@
 def solution(n):
     return sum(filter(lambda x: not n % x, [_ for _ in range(1, n+1)]))
+
+if __name__=="__main__":
+    print(f'Input: {12} | MyAns: {solution(12)} | Ans: {28}')
+    print(f'Input: {5} | MyAns: {solution(5)} | Ans: {6}')
