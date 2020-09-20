e1ebc989226f95fc17c850f1a0696cf2a790922d
1pha <62973169+1pha@users.noreply.github.com>
2020-09-20 15:19:27 +0900

    [Create] h-index.py(+11)

---
+++ lv2/h-index.py
@@ -0,0 +1,21 @@
+def solution(citations):
+    
+    for ref in range(len(citations), 0, -1):
+        if ref <= len(list(filter(lambda x: x >= ref, citations))): return ref
+        
+    return 0
+    
+    
+if __name__=="__main__":
+    
+    citations = [3, 0, 6, 1, 5]
+    print(f'citations: {citations} | sol: {solution(citations)} | ans: 3')
+    
+    citations = [5, 5, 5, 5]
+    print(f'citations: {citations} | sol: {solution(citations)} | ans: 4')
+    
+    citations = [31, 66]
+    print(f'citations: {citations} | sol: {solution(citations)} | ans: 3')
+    
+    citations = [0, 1, 3, 5, 5, 5, 5, 5, 6]
+    print(f'citations: {citations} | sol: {solution(citations)} | ans: 5')
