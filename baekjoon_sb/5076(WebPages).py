7b973b817d1dd73d11f80976ed9b1c485bc96a33
Daehyun Cho <1phantasmas@korea.ac.kr>
2020-10-25 00:13:11 +0900

    [Update] 5076 solved!

--- baekjoon_sb/5076(WebPages).py
+++ baekjoon_sb/5076(WebPages).py
@@ -1,29 +1,37 @@
-def check_stack(stack):
-    pass
-
-
 while True:
 
     try:
         web = input()
+        if web == '#': break
 
         gather = False
-        tag = []
-        stack = []
+        tag, stack = [], []
         for s in web:
 
+            # When Tag starts, start to gather tag name
+            # when gather false, don't receive any inputs...
             if s == '<':
                 gather = True
 
+            # When Tag ends
             elif s == '>':
 
                 gather = False
 
+                # Normal tags have tag, /tag structure -> unify without /
                 ele = ''.join(tag).lstrip('/')
+                
+                # when last string is /, it is self opening-closing tag, kill
                 if ele[-1] == '/': 
                     tag = []
                     continue
 
+                # when it is splitable, it has attributes
+                # use the first one only
+                elif len(ele.split()) > 1:
+                    ele = ele[0]
+
+                # check stack
                 if stack:
                     if stack[-1] == ele:
                         stack.pop(-1)
@@ -33,18 +41,17 @@ while True:
 
                 else:
                     stack.append(ele)
-                #stack.append(''.join(tag).lstrip('/'))
+
+                # when stack is done, reset tag
                 tag = []
 
-            else:
-                if s == ' ':
-                    gather = False
-                    continue
 
+            # gather strings when only < is opened(gather=True)
+            else:
                 if gather:
                     tag.append(s)
             
-        print(stack)
+        # printing part
         if stack:
             print('illegal')
 
