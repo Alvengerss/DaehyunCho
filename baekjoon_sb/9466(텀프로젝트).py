795ef1c0a14f4152b29a6e40ee1ddf66a2b89d9f
Daehyun Cho <1phantasmas@korea.ac.kr>
2020-11-12 12:40:06 +0900

    [9466] working

---
+++ baekjoon_sb/9466(텀프로젝트).py
@@ -0,0 +1,21 @@
+N = int(input())
+
+def graph_search(teammates):
+
+    graph = dict()
+
+    src = 0
+    while True:
+
+        dst = teammates[src]
+        
+
+
+
+for case in range(N):
+
+    students = int(input()):
+    teams = []
+    teammates = list(map(int, input().split())
+
+    
